1a908aa7c380bfd69eae627b0504f94672415cf3
als698 <39515296+als698@users.noreply.github.com>
2020-05-11 21:14:24 +0300

    timer func

---
+++ app/src/main/java/com/example/timerproj/MainActivity.kt
@@ -0,0 +1,177 @@
+package com.example.timerproj
+
+import android.content.IntentSender
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.timerproj.util.PrefUtil
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class MainActivity : AppCompatActivity() {
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds = 0L
+    private var timerState= TimerState.Stopped
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_av)
+        supportActionBar?.title = "     Timer"
+
+        fab_play.setOnClickListener { v ->
+            startTimer()
+                 TimerState = TimerState.Running
+            updateButtons()
+        }
+
+        fab_pause.setOnClickListener { v->
+            timer.cancel()
+            timerState = TimerState.Paused
+        }
+
+        fab_stop.setOnClickListener { v->
+            timer.cancel()
+            onTimerFinished()
+        }
+
+        private fun initTimer() {
+            timerState = PrefUtil.getTimerState(context = this)
+
+            if(timerState == TimerState.Stopped)
+                setNewTimerLength()
+            else
+                setPreviousTimerLength()
+
+            secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+                prefUtil.getSecondsRemaining(context = this)
+            else
+                timerLengthSeconds
+
+            if(timerState == TimerState.Running)
+                startTimer()
+
+            updateButtons()
+            updateCountdownUI()
+        }
+
+        override fun onResume() {
+            super.onResume()
+
+            initTimer()
+
+            //sters bkg
+        }
+
+        override fun onPause() {
+            super.onPause()
+
+            if(timerState == TimerState.Running) {
+                timer.cancel()
+                //start bkg si afisare notif
+            } else if (timerState == TimerState.Paused) {
+                //afisare notificari
+            }
+
+            PrefUtil.setPreviousTimerLenghtSeconds(timerLengthSeconds)
+            PrefUtil.setSecondsRemaining(secondsRemaining, context = this)
+            PrefUtil.setTimerState(timerState, context = this)
+        }
+
+
+
+    }
+
+    private fun updateCountdownUI() {
+        val minutesUntilFinished = (secondsRemaining / 60)
+        val minutesUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        var secondsStr = secondsInMinutesUntilFinished.toString()
+        textView_countdown.text = "$minutesUntilFinished:${
+        if (secondsStr.lenth == 2) secondsStr
+        else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+    private fun onTimerFinished() {
+        timerState = TimerState.Stopped
+
+        setNewTimerLength()
+
+        progress_countdown.progress = 0
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, context = this)
+        secondsRemaining = timerLengthSeconds
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private var secondsRemaining = 0L
+    private fun startTimer() {
+        timerState = TimerState.Running
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000 ) {
+            override fun onFinish() = onTimerFinished()
+            override fun onTick(milisUntilFinished: Long) {
+                secondsRemaining = milisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength() {
+        val lengthInMinutes = PrefUtil.getTimerLength(context = this)
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(context = this)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+
+
+    private fun updateButtons() {
+        when (timerState) {
+            TimerState.Running ->{
+                fab_play.isEnabled = false
+                fab_pause.isEnabled = true
+                fab_stop.isEnabled = true
+            }
+            TimerState.Stopped ->{
+                fab_play.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = false
+            }
+            TimerState.Paused ->{
+                fab_play.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = true
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/timerproj/util/PrefUtil.kt
@@ -0,0 +1,56 @@
+package com.example.timerproj.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import java.util.prefs.PreferenceChangeEvent
+
+class PrefUtil {
+    companion object {
+        fun getTimerLength(context: Context): Int {
+            return 1
+        }
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.rescoder.timer.prevois_timer_length"
+
+        fun getPreviousTimerLengthSeconds(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, defValue:0)
+        }
+
+        fun setPreviousTimerLenghtSeconds(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "com.rescoder.timer.timer_state"
+
+        fun getTimerState(context: Context): ActivitateTimer.TimerState {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, defValue=0)
+            return ActivitateTimer.TimerState.values()[ordinal]
+        }
+
+        fun setTimerState(state: ActivitateTimer.TimerState, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+
+        }
+
+        private const val SECONDS_REMAINING_ID = "com.rescoder.timer.seconds_remaining"
+
+        fun getSecondsRemaining(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, defValue=0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+
+    }
+}
